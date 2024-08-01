8d60db51610318655c6b44d996254858972bad5c
Julien Denizot <luunz71@gmail.com>
2024-08-01 11:58:54 +0200

    fix

--- bot.py
+++ bot.py
@@ -3,7 +3,10 @@ from discord.ext import commands
 import asyncio
 from dotenv import load_dotenv
 import os
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("Bot is starting...")
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
